9980fd834f0bc80a09f2225071d84d4af0113d52
fashniyo <olaniyifashola14@gmail.com>
2020-07-31 12:25:42 +0100

    setup validator and validate add music

--- routes/music.js
+++ routes/music.js
@@ -2,10 +2,12 @@ import express from 'express'
 // eslint-disable-next-line import/no-named-as-default
 import Music from '../plugins/music'
 
+import validateAddMusic from '../validators/AddMusicValidator'
+
 const router = express.Router()
 
 router.get('/music', Music.getAllMusic)
-router.post('/music', Music.addMusic)
+router.post('/music', validateAddMusic, Music.addMusic)
 router.delete('/music/:id', Music.deleteMusic)
 router.put('/music/:id', Music.updateMusic)
 router.get('/musics/title', Music.searchTitle)
