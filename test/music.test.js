1ca95e5059868b2706ab0b83b211a6a2926d436b
fashniyo <olaniyifashola14@gmail.com>
2020-08-23 15:35:56 +0100

    add music test

--- test/music.test.js
+++ test/music.test.js
@@ -1,22 +1,59 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import { expect } from 'chai'
 import supertest from 'supertest'
 import server from '../app'
+import models from '../models'
 
 const chai = require('chai')
 
 const should = chai.should()
 
 const request = supertest.agent(server)
+const MusicModel = models.Music
 
 // eslint-disable-next-line no-undef
-describe('Index route', () => {
-  // eslint-disable-next-line no-undef
-  it('should return welcome message when / route is matched', (done) => {
-    request.get('/').end((err, res) => {
-      res.status.should.be.equal(200)
-      expect(res.body.message).be.equal('Welcome to Music House Api')
-      done()
+describe('Music Api', () => {
+  before(async () => {
+    // create database tables
+    await models.sequelize.sync()
+
+    await MusicModel.create({
+      title: 'music test',
+      album: 'album test',
+      genres: 'RnB',
+      length: '10:50',
+      rating: 7,
+      likes: 1000,
+      year: 2050,
+      artist: 'artist test',
+      producer: 'producer test',
+      video: 'avalible',
+      lyrics: 'hdgvvs dhsnans dfbfhsh'
+    })
+  })
+  after(async () => {
+    // empty the database
+    await MusicModel.destroy({ where: {} })
+  })
+
+  describe('Index route', () => {
+    it('should return welcome message when / route is matched', (done) => {
+      request.get('/').end((err, res) => {
+        res.status.should.be.equal(200)
+        expect(res.body.message).be.equal('Welcome to Music House Api')
+        done()
+      })
+    })
+    describe('/GET Get all music', () => {
+      it('it should GET all the music', (done) => {
+        request.get('/music').end((err, res) => {
+          res.status.should.be.equal(200)
+          expect(res.body.music).to.be.an('array')
+          expect(res.body.message).be.equal('Music fetched successfully')
+          done()
+        })
+      })
     })
   })
 })
