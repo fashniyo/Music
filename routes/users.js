2537a583fbc57ec711a9ca87045de73937da6ea7
fashniyo <olaniyifashola14@gmail.com>
2020-08-26 22:38:51 +0100

    sign in validator, test, route and plugins

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import User from '../plugins/users'
 import validateSignup from '../validators/validateUserSignUp'
+import validateSignIn from '../validators/validateSignIn'
 
 const router = express.Router()
 
 router.post('/signup', validateSignup, User.signUp)
+router.post('/signin', validateSignIn, User.signIn)
 
 module.exports = router
