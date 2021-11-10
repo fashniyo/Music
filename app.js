afbd2d1e5cfe5f303ca9dea1f70207dc99e1c27a
OLANIYI FASHOLA <olaniyifashola14@gmail.com>
2021-11-11 00:26:40 +0100

    updating packages and app js

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ import musicRouter from './routes/music'
 
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
